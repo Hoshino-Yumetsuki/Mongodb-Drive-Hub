1814141bfea6c5303c7c36eee336e884c2262b0a
Anjiurine <q78kg.1ffrj6@outlook.com>
2023-11-15 22:49:32 +0800

    add app

---
+++ app/src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app').$nextTick(() => {
+  // Remove Preload scripts loading
+  postMessage({ payload: 'removeLoading' }, '*')
+
+  // Use contextBridge
+  window.ipcRenderer.on('main-process-message', (_event, message) => {
+    console.log(message)
+  })
+})
