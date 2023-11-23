20340e2e645a0ff8078a0331445f4a1f016951fc
Anjiurine <q78kg.1ffrj6@outlook.com>
2023-11-23 22:22:11 +0800

    support web download

--- mdh.py
+++ mdh.py
@@ -1,26 +1,9 @@
-from utils import user_io
-from utils import mongo_utils
-import json
-import os
-import traceback
-import logging
-from concurrent.futures import ThreadPoolExecutor
 import sys
+from utils.servermod import run_server_mode
+from utils.clientmod import run_cli_mode
 
 if __name__ == "__main__":
-    try:
-        if os.path.exists('./uri_list.json') == False:
-            print("uri_list.json not found")
-            sys.exit()
-        with open("uri_list.json", "r") as f:
-            uri_list = json.load(f)["uri_list"]
-        client_list = mongo_utils.connect_mongo_cluster(uri_list)
-        user_io.print_welcome()
-        while True:
-            user_input = input("Please enter the command：")
-            with ThreadPoolExecutor() as executor:
-                futures = [executor.submit(user_io.parse_input, user_input, client_list)]
-                for future in futures:
-                    future.result()
-    except Exception as e:
-        logging.error(f"An unexpected error occurred: {str(e)}\n{traceback.format_exc()}")
+    if "-s" in sys.argv or "--server" in sys.argv:
+        run_server_mode()
+    else:
+        run_cli_mode()
