a3d1a9b6217419ebb4fcd2ffc8dad8a9efc9d660
Anjiurine <q78kg.1ffrj6@outlook.com>
2023-11-25 12:01:55 +0800

    .

--- mdh.py
+++ mdh.py
@@ -1,6 +1,5 @@
 import sys
-from utils.servermod import run_server_mode
-from utils.clientmod import run_cli_mode
+from utils.service import run_server_mode, run_cli_mode
 
 if __name__ == "__main__":
     if "-s" in sys.argv or "--server" in sys.argv:
--- utils/service.py
+++ utils/service.py
@@ -1,16 +1,48 @@
+from . import mongo_utils, user_io
+import json
+import os
+import traceback
+import logging
+from concurrent.futures import ThreadPoolExecutor
+import sys
 import atexit
 import signal
 from flask import Flask, send_file, after_this_request
-import os
-from . import mongo_utils
-import logging
-import sys
-import json
-import traceback
-
-app = Flask(__name__)
 
 client_list = []
+
+def remove_temp_files():
+    save_path = "./cache/"
+    for file_name in os.listdir(save_path):
+        file_path = os.path.join(save_path, file_name)
+        try:
+            os.unlink(file_path)
+        except Exception as e:
+            logging.warning(f"Error removing temporary file: {str(e)}")
+
+def run_cli_mode():
+    try:
+        if os.path.exists('./uri_list.json') == False:
+            print("uri_list.json not found")
+            sys.exit()
+        with open("uri_list.json", "r") as f:
+            uri_list = json.load(f)["uri_list"]
+        client_list = mongo_utils.connect_mongo_cluster(uri_list)
+        user_io.print_welcome()
+        while True:
+            user_input = input("Please enter the command：")
+            with ThreadPoolExecutor() as executor:
+                futures = [executor.submit(user_io.parse_input, user_input, client_list)]
+                for future in futures:
+                    future.result()
+    except KeyboardInterrupt:
+        print("\nProgram terminated by user.")
+        remove_temp_files()
+        sys.exit(0)
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {str(e)}\n{traceback.format_exc()}")
+
+app = Flask(__name__)
 
 def remove_temp_files():
     save_path = "./cache/"
